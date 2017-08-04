d4f4fa02644c6bf9d97260fda08f9c28acfd720e
picocoder <dancetack3@yahoo.com>
2017-08-04 13:37:46 -0600

    Add files via upload

--- largerhud/init.lua
+++ largerhud/init.lua
@@ -1,1 +1,19 @@
 --this is a new mod idea, not implemented yet
+    minetest.register_on_joinplayer(function(player)
+      
+      player:hud_set_hotbar_image("customhb_gui_hotbar.png")
+      player:hud_set_hotbar_selected_image("customhb_gui_hotbar_selected.png")
+      end)
+
+minetest.register_chatcommand("hotbar_size", {
+    param = "<htb>",
+	func = function(name, htb)
+        if (htb == nil) then
+            minetest.chat_send_player(name, message)
+        end
+		local player = minetest.get_player_by_name(name)
+		player:set_physics_override({
+			player:hud_set_hotbar_itemcount(htb)
+		})
+    end
+})
--- speed/init.lua
+++ speed/init.lua
@@ -1,8 +1,8 @@
 minetest.register_chatcommand("speed", {
     param = "<spe>",
 	func = function(name, spe)
-        if (spe == nil) then 
-            spe=100
+        if (spe == nil) then
+            minetest.chat_send_player(name, message)
         end
 		local player = minetest.get_player_by_name(name)
 		player:set_physics_override({
@@ -10,3 +10,7 @@ minetest.register_chatcommand("speed", {
 		})
     end
 })
+
+--minetest.register_chatcommand("speed", {
+--        if (spe == nil) then 
+--            minetest.chat_send_player(player:get_player_name(), "You said: " .. fields.name .. "!")
